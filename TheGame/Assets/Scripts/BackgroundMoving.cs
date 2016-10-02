6bae033177b4f4d0a45dac308b1e8beaa39d15f0
nikigmy <nikigmy@gmail.com>
2016-10-03 00:18:46 +0300

    Tested some stuff

---
+++ TheGame/Assets/Scripts/BackgroundMoving.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BackgroundMoving : MonoBehaviour
+{
+    private SpawningTerain spawningTerain;
+    private bool spawned;
+	void Start ()
+	{
+	    spawned = false;
+	    spawningTerain = GameObject.Find("Utility").GetComponent<SpawningTerain>();
+	}
+	
+	void Update () {
+	    transform.Translate(-0.1f, 0, 0);
+        
+        if (Math.Abs(transform.position.x) < 0.2 && spawned == false)
+	    {
+            spawned = true;
+            spawningTerain.SpawnBackground();
+	        spawned = true;
+	    }
+	    if (Math.Abs(transform.position.x + 22.5f) < 0.2)
+	    {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ TheGame/Assets/Scripts/PlatformMoving.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMoving : MonoBehaviour {
+    
+	void Update () {
+        transform.Translate(-0.1f, 0, 0);
+    }
+}
