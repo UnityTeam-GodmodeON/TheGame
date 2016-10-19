3a21dd3316b69da4f44d5faf6bdc8ccc97c28ca1
nikigmy <nikigmy@gmail.com>
2016-10-19 13:40:26 +0300

    Created a menu. Please someone fix the designgit status !

---
+++ TheGame/Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueButton : MonoBehaviour
+{
+	void Start ()
+	{
+	    if (!PlayerPrefs.HasKey("GameProgress"))
+	    {
+            gameObject.SetActive(false);
+	    }
+	    
+	}
+}
