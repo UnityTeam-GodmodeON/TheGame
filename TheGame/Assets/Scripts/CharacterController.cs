eaa3a258d6c688b92faff09a4b93c56b823170ff
nikigmy <nikigmy@gmail.com>
2016-11-09 19:18:10 +0200

    Added some scripts and UI for the Game Scene.

---
+++ TheGame/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+    private Hero hero;
+	void Start ()
+	{
+	    hero = GetComponent<Hero>();
+	}
+	
+	void Update () {
+        
+    }
+
+    public void BlockClicked()
+    {
+        
+    }
+
+    public void Walk(float direction)
+    {
+        gameObject.transform.Translate(Vector3.right * direction);
+        hero.Walk();
+    }
+
+    public void AttackClicked()
+    {
+        hero.Attack();
+    }
+
+    public void SpecialAttackClicked()
+    {
+        hero.SpecialAttack();
+    }
+}
--- TheGame/Assets/Scripts/Hero.cs
+++ TheGame/Assets/Scripts/Hero.cs
@@ -1,15 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
+    public int Health;
+    private bool blocking;
+    private int Cooldown; //Will be something different
+    private bool Walking;
+    private Animator anim;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        Health = 100;
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (Health <= 0)
+        {
+            Die();
+        }
+        anim.SetBool("Moving", Walking);
+        Walking = false;
+    }
+
+    public void TakeDamage(int value, string attackType)
+    {
+        int damage = value;
+        if (attackType == "normal")
+        {
+            damage = damage / 2;
+        }
+        Health -= damage;
+    }
+
+    public void Block()
+    {
+        //anim.SetBool("Blocking", true);
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("Death");
+    }
+
+    public void SpecialAttack()
+    {
+        if (Cooldown <= 0)
+        {
+            anim.SetTrigger("SpecialAttack");
+        }
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    public void Walk()
+    {
+        Walking = true;
+    }
 }
